c66524ecd6faff39700f4d31eb9beed2d808c523
SZRSF <80523364+SZRSF@users.noreply.github.com>
2023-10-13 09:45:13 +0800

    登录功能完成！

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -9,8 +9,8 @@ public interface EmployeeMapper {
 
     /**
      * 根据用户名查询员工
-     * @param username
-     * @return
+     * @param username 用户名
+     * @return 返回查询结果
      */
     @Select("select * from employee where username = #{username}")
     Employee getByUsername(String username);
