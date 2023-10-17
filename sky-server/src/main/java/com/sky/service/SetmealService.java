3cc5564768c987020b224cc0b80bd5e6751d372d
SZRSF <80523364+SZRSF@users.noreply.github.com>
2023-10-17 10:22:58 +0800

    套餐分页查询

--- sky-server/src/main/java/com/sky/service/SetmealService.java
+++ sky-server/src/main/java/com/sky/service/SetmealService.java
@@ -1,6 +1,8 @@
 package com.sky.service;
 
 import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
+import com.sky.result.PageResult;
 
 /**
  * 套餐业务接口
@@ -15,4 +17,12 @@ public interface SetmealService {
      * @param setmealDTO 套餐信息
      */
     void save(SetmealDTO setmealDTO);
+
+    /**
+     * 套餐分页查询业务接口
+     *
+     * @param setmealPageQueryDTO 套餐分页查询参数
+     * @return 返回分页查询结果
+     */
+    PageResult pageQuery(SetmealPageQueryDTO setmealPageQueryDTO);
 }
--- sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
@@ -1,10 +1,14 @@
 package com.sky.service.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
 import com.sky.entity.Setmeal;
 import com.sky.entity.SetmealDish;
 import com.sky.mapper.SetmealDishMapper;
 import com.sky.mapper.SetmealMapper;
+import com.sky.result.PageResult;
 import com.sky.service.SetmealService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
@@ -59,5 +63,22 @@ public class SetmealServiceImpl implements SetmealService {
             // 5.将套餐关联的菜品数据插入套餐菜品关联表中
             setmealDishMapper.insertBatch(setmealDishList);
         }
+    }
+
+    /**
+     * 套餐分页查询
+     *
+     * @param setmealPageQueryDTO 套餐分页查询参数
+     * @return 返回分页查询结果
+     */
+    @Override
+    public PageResult pageQuery(SetmealPageQueryDTO setmealPageQueryDTO) {
+        // 1.设置分页参数
+        PageHelper.startPage(setmealPageQueryDTO.getPage(), setmealPageQueryDTO.getPageSize());
+        // 2.进行分页查询
+        Page<Setmeal> page = setmealMapper.pageQuery(setmealPageQueryDTO);
+
+        // 3.返回查询结果
+        return new PageResult(page.getTotal(), page.getResult());
     }
 }
