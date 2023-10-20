c8f7c64b944836ec4906483769188dc66e0b638e
SZRSF <80523364+SZRSF@users.noreply.github.com>
2023-10-20 14:46:57 +0800

    缓存套餐业务功能代码开发

--- sky-server/src/main/java/com/sky/controller/user/DishController.java
+++ sky-server/src/main/java/com/sky/controller/user/DishController.java
@@ -9,6 +9,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,6 +30,9 @@ public class DishController {
     @Autowired
     private DishService dishService;
 
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
     /**
      * 根据分类id查询菜品
      *
@@ -38,12 +42,28 @@ public class DishController {
     @ApiOperation("根据分类id查询菜品")
     @GetMapping("/list")
     public Result<List<DishVO>> list(Long categoryId) {
+        log.info("根据分类id查询菜品:{}", categoryId);
+
+        // 构建redis中的key， 规则：dish_分类id
+        String key = "dish_" + categoryId;
+
+        // 查询redis中是否存在菜品数据
+        List<DishVO> list = (List<DishVO>) redisTemplate.opsForValue().get(key);
+        if (list != null && list.size() > 0) {
+            // 如果存在，直接返回，无需查询数据库
+            return Result.success(list);
+        }
+
         Dish dish = new Dish();
         dish.setCategoryId(categoryId);
         // 查询起售中的菜品
         dish.setStatus(StatusConstant.ENABLE);
 
-        List<DishVO> list = dishService.listWithFlavor(dish);
+        // 如果不存在，查询数据库，将查询到的数据放入redis中
+        list = dishService.listWithFlavor(dish);
+
+        // 放入到redis中
+        redisTemplate.opsForValue().set(key,list);
 
         return Result.success(list);
     }
--- sky-server/src/main/java/com/sky/controller/user/SetmealController.java
+++ sky-server/src/main/java/com/sky/controller/user/SetmealController.java
@@ -7,7 +7,9 @@ import com.sky.service.SetmealService;
 import com.sky.vo.DishItemVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +23,7 @@ import java.util.List;
 @RestController("userSetmealController")
 @RequestMapping("/user/setmeal")
 @Api(tags = "C端-套餐浏览接口")
+@Slf4j
 public class SetmealController {
 
     @Autowired
@@ -34,7 +37,9 @@ public class SetmealController {
      */
     @GetMapping("/list")
     @ApiOperation("根据分类id查询套餐")
+    @Cacheable(cacheNames = "setmealCache", key = "#categoryId")
     public Result<List<Setmeal>> list(Long categoryId) {
+        log.info("根据分类id查询套餐:{}",categoryId);
         Setmeal setmeal = new Setmeal();
         setmeal.setCategoryId(categoryId);
         setmeal.setStatus(StatusConstant.ENABLE);
@@ -52,6 +57,7 @@ public class SetmealController {
     @GetMapping("/dish/{id}")
     @ApiOperation("根据套餐id查询包含的菜品列表")
     public Result<List<DishItemVO>> dishList(@PathVariable("id") Long id) {
+        log.info("根据套餐id查询包含的菜品列表:{}",id);
         List<DishItemVO> list = setmealService.getDishItemById(id);
         return Result.success(list);
     }
