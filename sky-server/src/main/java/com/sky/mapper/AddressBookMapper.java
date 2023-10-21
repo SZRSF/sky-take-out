c7a202cf808d7a62599ef468b8137ce256651013
SZRSF <80523364+SZRSF@users.noreply.github.com>
2023-10-21 16:05:17 +0800

    地址簿接口代码开发完成

--- sky-server/src/main/java/com/sky/mapper/AddressBookMapper.java
+++ sky-server/src/main/java/com/sky/mapper/AddressBookMapper.java
@@ -1,8 +1,9 @@
 package com.sky.mapper;
 
 import com.sky.entity.AddressBook;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
 
 /**
  * @author zengzhicheng
@@ -20,4 +21,45 @@ public interface AddressBookMapper {
             "values (#{userId}, #{consignee}, #{phone}, #{sex}, #{provinceCode}, #{provinceName}, #{cityCode}, " +
             "#{cityName}, #{districtCode}, #{districtName}, #{detail}, #{label}, #{isDefault})")
     void insert(AddressBook addressBook);
+
+
+    /**
+     * 条件查询
+     *
+     * @param addressBook
+     * @return
+     */
+    List<AddressBook> list(AddressBook addressBook);
+
+    /**
+     * 根据id查询
+     *
+     * @param id
+     * @return
+     */
+    @Select("select * from address_book where id = #{id}")
+    AddressBook getById(Long id);
+
+    /**
+     * 根据id修改
+     *
+     * @param addressBook
+     */
+    void update(AddressBook addressBook);
+
+    /**
+     * 根据 用户id修改 是否默认地址
+     *
+     * @param addressBook
+     */
+    @Update("update address_book set is_default = #{isDefault} where user_id = #{userId}")
+    void updateIsDefaultByUserId(AddressBook addressBook);
+
+    /**
+     * 根据id删除地址
+     *
+     * @param id
+     */
+    @Delete("delete from address_book where id = #{id}")
+    void deleteById(Long id);
 }
--- sky-server/src/main/java/com/sky/service/AddressBookService.java
+++ sky-server/src/main/java/com/sky/service/AddressBookService.java
@@ -2,6 +2,8 @@ package com.sky.service;
 
 import com.sky.entity.AddressBook;
 
+import java.util.List;
+
 /**
  * 地址簿业务接口
  *
@@ -15,4 +17,42 @@ public interface AddressBookService {
      * @param addressBook
      */
     void save(AddressBook addressBook);
+
+    /**
+     * 查询当前登录对象的所有地址信息
+     *
+     * @param addressBook
+     * @return
+     */
+    List<AddressBook> list(AddressBook addressBook);
+
+    /**
+     * 根据Id查询地址
+     *
+     * @param id
+     * @return
+     */
+    AddressBook getById(Long id);
+
+    /**
+     * 跟新地址
+     *
+     * @param addressBook
+     */
+    void update(AddressBook addressBook);
+
+    /**
+     * 设置默认地址
+     *
+     * @param addressBook
+     */
+    void setDefault(AddressBook addressBook);
+
+    /**
+     * 根据Id删除地址
+     *
+     * @param id
+     */
+    void deleteById(Long id);
+
 }
